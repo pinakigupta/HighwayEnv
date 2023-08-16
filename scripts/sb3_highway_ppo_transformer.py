7cb09e09875aa143838afb3e7247a045e65e2466
Pinaki Gupta <gpinaki@yahoo.com>
2023-08-16 07:30:10 -0700

    some more changes to expert data collection and print

--- scripts/sb3_highway_ppo_transformer.py
+++ scripts/sb3_highway_ppo_transformer.py
@@ -62,7 +62,7 @@ class TrainEnum(Enum):
     BC = 5
     BCDEPLOY = 6
 
-train = TrainEnum.BCDEPLOY
+train = TrainEnum.BC
 
 def append_key_to_dict_of_dict(kwargs, outer_key, inner_key, value):
     kwargs[outer_key] = {**kwargs.get(outer_key, {}), inner_key: value}
@@ -692,10 +692,10 @@ if __name__ == "__main__":
         conf_matrix = confusion_matrix(true_labels, predicted_labels)
 
         # Print the metrics
-        print("Accuracy:", accuracy)
-        print("Precision:", precision)
-        print("Recall:", recall)
-        print("F1 Score:", f1)
+        print("Accuracy:", accuracy, np.mean(accuracy))
+        print("Precision:", precision, np.mean(precision))
+        print("Recall:", recall, np.mean(recall))
+        print("F1 Score:", f1, np.mean(f1))
 
 
         with torch.no_grad():
@@ -704,17 +704,17 @@ if __name__ == "__main__":
 
         # Calculate evaluation metrics
         accuracy = accuracy_score(true_labels, predicted_labels)
-        precision = precision_score(true_labels, predicted_labels, average='macro')
-        recall = recall_score(true_labels, predicted_labels, average='macro')
-        f1 = f1_score(true_labels, predicted_labels, average='macro')
+        precision = precision_score(true_labels, predicted_labels, average=None)
+        recall = recall_score(true_labels, predicted_labels, average=None)
+        f1 = f1_score(true_labels, predicted_labels, average=None)
 
         # Print the Training metrics
 
         print("--------  Training data metrics for reference---------------")
-        print("Accuracy:", accuracy)
-        print("Precision:", precision)
-        print("Recall:", recall)
-        print("F1 Score:", f1)
+        print("Accuracy:", accuracy, np.mean(accuracy))
+        print("Precision:", precision,  np.mean(precision))
+        print("Recall:", recall, np.mean(recall))
+        print("F1 Score:", f1, np.mean(recall))
 
 
         from highway_env.envs.common.action import DiscreteMetaAction
