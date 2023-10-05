a13ef253a49434ec5070b8118f1758c06d149a6c
pinakigupta <gpinaki@yahoo.com>
2023-10-05 03:14:30 +0000

    val batch count config change

--- scripts/sb3_highway_ppo_transformer.py
+++ scripts/sb3_highway_ppo_transformer.py
@@ -694,7 +694,7 @@ if __name__ == "__main__":
                                                                             batch_size=batch_size,
                                                                             n_cpu=n_cpu,
                                                                             visited_data_files=[],
-                                                                            val_batch_count=500,
+                                                                            val_batch_count=5000,
                                                                             chunk_size=500,
                                                                             plot_path=None
                                                                           )
